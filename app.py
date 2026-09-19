71c865c29c48ae1243cfe77a935d67aff8318337
backrooms9000 <Geancaio926@gmail.com>
2026-09-18 21:47:57 -0300

    secret

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-from flask import Flask 
- 
-app = Flask(__name__) 
- 
-@app.route("/") 
-def hello_world(): 
-    return "<p>Hello, World!</p>" 
+from flask import Flask, render_template
+
+sitename = "My Flask"
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template(
+        "_base.html",
+        sitename=sitename
+    )
+     
 
 @app.route("/about")
-def about(): 
-    output = "algumacoisa"
-    return output
+def about():
+    return render_template( 
+        'about.html',
+        sitename=sitename
+   )
 
-if __name__ == "__main__": 
-    app.run(debug=True) 
 
- 
+if __name__ == "__main__":
+    app.run(debug=True)
